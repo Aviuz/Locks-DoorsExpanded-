a1a38746a9f332252cbf11ca5dc11f4191c1955d
Avius <avius.ravis@gmail.com>
2018-03-31 15:27:17 +0200

    fixed not working bug

--- Source/LockUtility.cs
+++ Source/LockUtility.cs
@@ -43,9 +43,10 @@ namespace LocksDoorsExpanded
             }
         }
 
-        public static bool PawnCanOpen(Building_DoorExpanded door, Pawn p)
+        public static bool PawnCanOpen(Building_Door doorHandler, Pawn p)
         {
             Lord lord = p.GetLord();
+            var door = ((Building_DoorRegionHandler)doorHandler).ParentDoor;
 
             bool canOpenAnyDoor = lord != null && lord.LordJob != null && lord.LordJob.CanOpenAnyDoor(p);
             bool noFaction = door.Faction == null;
